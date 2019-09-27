615a1b465dd42d0990ba53d57418e17f4bee8390
lxy764139720 <764139720@qq.com>
2019-09-27 21:54:59 +0800

    更新一下

---
+++ sources/com/androlua/BuildConfig.java
@@ -0,0 +1,10 @@
+package com.androlua;
+
+public final class BuildConfig {
+    public static final String APPLICATION_ID = "com.androlua";
+    public static final String BUILD_TYPE = "release";
+    public static final boolean DEBUG = false;
+    public static final String FLAVOR = "";
+    public static final int VERSION_CODE = 4305;
+    public static final String VERSION_NAME = "4.3.5";
+}
---
+++ sources/com/androlua/C0364R.java
@@ -0,0 +1,27 @@
+package com.androlua;
+
+/* renamed from: com.androlua.R */
+public final class C0364R {
+
+    /* renamed from: com.androlua.R$drawable */
+    public static final class drawable {
+        public static final int icon = 2130771968;
+        public static final int welcome = 2130771969;
+    }
+
+    /* renamed from: com.androlua.R$string */
+    public static final class string {
+        public static final int accsibility_service_description = 2130837504;
+    }
+
+    /* renamed from: com.androlua.R$style */
+    public static final class style {
+        public static final int app_theme = 2130903040;
+    }
+
+    /* renamed from: com.androlua.R$xml */
+    public static final class xml {
+        public static final int accessibility_service_config = 2130968576;
+        public static final int androlua_filepaths = 2130968577;
+    }
+}
